a2ed5c57f0d2d30021c2540f692d74d2e04b3be1
Avior <florian.bouillon@delta-wings.net>
2021-02-17 09:24:45 +0100

    pouet
    Signed-off-by: Avior <florian.bouillon@delta-wings.net>

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
-	entry: './src/index.ts',
+	entry: './src/v2.ts',
 	plugins: [new webpack.ProgressPlugin()],
 
 	module: {
